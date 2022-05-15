b5970a8edbe68346c7c813b7c9f7f4624215c249
DanteAlucard98 <dantealucard98.dp@gmail.com>
2022-05-15 17:58:32 -0500

    el buenote

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:camera_filters/camera_filters.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Camera'),
+        ),
+        body: CameraScreenPlugin(
+          onDone: (value) {
+            /// value returns the picture path you can save here or navigate to some screen
+            print(value);
+          },
+
+          /// profileIconWidget: , if you want to add profile icon on camera you can your widget here
+
+          ///filterColor: ValueNotifier<Color>(Colors.transparent),  your first filter color when you open camera
+
+          /// filters: [],
+          ///you can pass your own list of colors like this List<Color> colors = [Colors.blue, Colors.blue, Colors.blue ..... Colors.blue]
+          ///make sure to pass transparent color to first index so the first index of list has no filter effect
+        ),
+      ),
+    );
+  }
+}
